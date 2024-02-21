945b854ea288b36320255480dc50136df439cfc0
유지민 <a01040987587@gmail.com>
2024-02-21 17:15:49 +0900

    02.21

--- 02800/02840/2846.cpp
+++ 02800/02840/2846.cpp
@@ -1,31 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
 int main() {
     int N;
     cin >> N;
+    vector<int> arr(N);
+    string input;
+    getline(cin, input);
 
-    vector<int> heights(N);
-    for (int i = 0; i < N; ++i) {
-        cin >> heights[i];
+    getline(cin, input);
+    istringstream iss(input);
+    for (int i = 0; i < N; i++)
+        iss >> arr[i];
+
+    int max = 0;
+    int temp = 0;
+    for (int i = 1; i < N; i++) {
+        if (arr[i - 1] < arr[i])
+            temp += arr[i] - arr[i - 1];
+        else
+            temp = 0;
+        max = max > temp ? max : temp;
     }
-
-    int max_uphill = 0;
-    int uphill_length = 0;
-    for (int i = 1; i < N; ++i) {
-        if (heights[i] > heights[i - 1]) {
-            uphill_length++;
-        } else {
-            max_uphill = max(max_uphill, uphill_length);
-            uphill_length = 0;
-        }
-    }
-
-    max_uphill = max(max_uphill, uphill_length);
-
-    cout << max_uphill << endl;
+    cout << max << endl;
 
     return 0;
 }
