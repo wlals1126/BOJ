429a00cb42c83c1f78f1bf82cf33d9096bc64182
유지민 <a01040987587@gmail.com>
2024-02-24 21:01:04 +0900

    주석 제거

--- 02400/02440/2447.cpp
+++ 02400/02440/2447.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 
 void printStars(int x, int y, int n) {
-    // 가운데를 제외한 영역에 별을 출력하거나 재귀 호출을 진행합니다.
     if ((x / n) % 3 == 1 && (y / n) % 3 == 1) {
         cout << ' ';
     } else {
@@ -22,7 +21,6 @@ int main() {
     int n;
     cin >> n;
     
-    // 크기 N의 패턴 출력
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             printStars(i, j, n);
