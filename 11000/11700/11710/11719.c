512f8535ce90461214f07747f96e4e39380004f2
유지민 <a01040987587@gmail.com>
2024-04-11 16:57:17 +0900

    0411(2)

--- 11000/11700/11710/11719.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main() {
-	char str;
-	while(scanf("%c", &str) != EOF) {
-		printf("%c", str);
-	}
-	return 0;
-}
--- README.md
+++
@@ -1,4 +0,0 @@
-# 백준 알고리즘 
-
-
-![mazandi profile](http://mazandi.herokuapp.com/api?handle=yjm1126&theme=cold)
